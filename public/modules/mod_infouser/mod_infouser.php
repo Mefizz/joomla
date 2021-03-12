7355e839a417b31c591e4ff1eebdb2846eda4632
Mefizz <58421356+Mefizz@users.noreply.github.com>
2021-03-12 17:48:14 +0200

    Update mod_infouser.php

--- public/modules/mod_infouser/mod_infouser.php
+++ public/modules/mod_infouser/mod_infouser.php
@@ -2,11 +2,11 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-// Отримуємо лінк на объект поточного документа
+
 $doc = &JFactory::getDocument();
 
 
-//Read module params
+
 $phrase = $params->get('phrase');
 $timer = $params->get('timer');
 
